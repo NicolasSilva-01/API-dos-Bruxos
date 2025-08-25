f3e1dde0e39078fb93dd0a0a6876b27db16856c3
Nicolas Silva <nicolas.m.silva19@aluno.senai.br>
2025-08-25 10:29:52 -0300

    feat:adiciona dados.js e modifica server.js

--- server.js
+++ server.js
@@ -1,20 +1,63 @@
 import express from "express";
-import bruxos from "./src/data/Bruxos .js";
+import dados from "./src/data/dados.js";
 
+const { bruxos, casas, varinhas, animais, pocoes } = dados;
 const app = express();
-const portServer = 3000;
+app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("Vamos de Harry Potter")
-})
+const serverPort = 3000;
 
-app.get("/bruxos", (req, res) => {
-    res.json(bruxos);
+app.get("/", (req, res) => {
+  res.send("🚀 Servidor funcionando...");
 });
 
 app.get("/bruxos", (req, res) => {
-    res.jason(bruxos);
+ if (bruxos.length > 0) {
+  res.status(200).json(bruxos);
+ } else {
+    res.status(404).json({
+        mensagem: "Nenhum bruxo encontrado!"
+         })
+    }
+});
+
+app.get("/casas", (req, res) => {
+  if (casas.length > 0) {
+    res.status(200).json(casas);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhuma casa encontrada!"
+        })
+    }
 })
-app.listen(portServer, () => {
-    console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000 !");
+app.get("/varinhas", (req, res) => {
+  if (varinhas.length > 0) {
+    res.status(200).json(varinhas);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhuma varinha encontrada!"
+        })
+    }
+})
+app.get("/animais", (req, res) => {
+  if (animais.length > 0) {
+    res.status(200).json(animais);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhum animal encontrado!"
+        })
+    }
+})
+app.get("/pocoes", (req, res) => {
+  if (pocoes.length > 0) {
+    res.status(200).json(pocoes);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhuma poção encontrada!"
+        })
+    }
+})
+
+app.listen(serverPort, () => {
+  console.log(`Servidor rodando em http://localhost:${serverPort} !!!`);
 });
